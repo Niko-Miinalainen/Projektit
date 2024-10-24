1c09658cfbd77fc2b3ccd940500118a346f4e3a4
Niko-Miinalainen <niko.miinalainen@edu.keuda.fi>
2024-10-24 11:08:59 +0300

    Korjauksia

--- 1A/kirjautumislomake.js
+++ 1A/kirjautumislomake.js
@@ -4,32 +4,33 @@ function tarkista(){
     let salaSana = document.getElementById("salasana").value
     let sahkoPosti = document.getElementById("sähköposti").value
     let palaute = document.getElementById("textarea").value
-    let checkBox1 = document.querySelector("radio").checked
-    let checkBox2 = document.querySelector("").checked
+    let checkBox1 = document.getElementById("radio").value
+    let checkBox2 = document.getElementById("checkbox").value
 
-    if (etuNimi.trim() = ""){
+    if (etuNimi.trim() == ""){
         alert("Etunimi puuttuu")
     }
-    else if (salaSana.trim() = ""){
-        alert("Salasana puuttuu")
-    }
-    else if (sahkoPosti.trim() = ""){
+    else if (sahkoPosti.trim() == ""){
         alert("Sähköposti puuttuu")
     }
-    else if (palaute.trim() = ""){
+    else if (salaSana.trim() == ""){
+        alert("Salasana puuttuu")
+    }
+    else if (palaute.trim() == ""){
         alert("Palaute on tyhjä")
     }
-    else if (checkBox1 = false){
+    else if (document.getElementById("radio").checked = false){
         alert("Valitse jotain")
     }
-    else if (checkBox2 = false){
+    else if (document.getElementById("checkbox").checked = false){
         alert("Valitse jotain")
     }
     else{
         alert("Lähetetty")
     }
+    event.preventDefault();
 }
 
 function tyhjenna(){
-    document.getElementById("").reset();
+    document.getElementById("form").reset();
 }
