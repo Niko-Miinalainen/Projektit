f12a9e282c7d1f4d08bb027797732212c4bfd335
aku-koivunen <aku.koivunen@edu.keuda.fi>
2024-10-30 10:00:22 +0200

    toimii

--- 1A/kirjautumislomake.js
+++ 1A/kirjautumislomake.js
@@ -1,40 +1,32 @@
-function tarkista(){
+function tarkista() {
+  let etuNimi = document.getElementById("nimi").value;
+  let salaSana = document.getElementById("salasana").value;
+  let sahkoPosti = document.getElementById("sähköposti").value;
+  let palaute = document.getElementById("textarea").value;
+  let radio1 = document.getElementById("radio1").checked;
+  let radio2 = document.getElementById("radio2").checked;
+  let checkBox1 = document.getElementById("checkbox1").checked;
+  let checkBox2 = document.getElementById("checkbox2").checked;
+  let checkBox3 = document.getElementById("checkbox3").checked;
 
-    let etuNimi = document.getElementById("nimi").value
-    let salaSana = document.getElementById("salasana").value
-    let sahkoPosti = document.getElementById("sähköposti").value
-    let palaute = document.getElementById("textarea").value
-    let radio1 = document.getElementById("radio1").checked
-    let radio2 = document.getElementById("radio2").checked
-    let checkBox1 = document.getElementById("checkbox1").checked
-    let checkBox2 = document.getElementById("checkbox2").checked
-    let checkBox3 = document.getElementById("checkbox3").checked
-    let checkBox4 = document.getElementById("checkbox4").checked
-
-    if (etuNimi.trim() == ""){
-        alert("Etunimi puuttuu")
-    }
-    else if (sahkoPosti.trim() == ""){
-        alert("Sähköposti puuttuu")
-    }
-    else if (salaSana.trim() == ""){
-        alert("Salasana puuttuu")
-    }
-    else if (palaute.trim() == ""){
-        alert("Palaute on tyhjä")
-    }
-    else if (radio1 || radio2 == false){
-        alert("Valitse jotain")
-    }
-    else if (checkBox1 || checkBox2 || checkBox3 || checkBox4 == true){
-        alert("Lähetetty")
-    }
-    else{
-        alert("Jotain puuttuu")
-    }
-    event.preventDefault();
+  if (etuNimi.trim() == "") {
+    alert("Etunimi puuttuu");
+  } else if (sahkoPosti.trim() == "") {
+    alert("Sähköposti puuttuu");
+  } else if (salaSana.trim() == "") {
+    alert("Salasana puuttuu");
+  } else if (palaute.trim() == "") {
+    alert("Palaute on tyhjä");
+  } else if (radio1 == false && radio2 == false) {
+    alert("Paljonko pidät roskapostista?");
+  } else if (checkBox1 == false && checkBox2 == false && checkBox3 == false) {
+    alert("Mistä pidit eniten?");
+  } else {
+    alert("Lähetys onnistu!");
+  }
+  event.preventDefault();
 }
 
-function tyhjenna(){
-    document.getElementById("form").reset();
+function tyhjenna() {
+  document.getElementById("form").reset();
 }
