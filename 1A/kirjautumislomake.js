929cd418d645dfc25c4dae98632f70ccb7f1dacf
Niko-Miinalainen <niko.miinalainen@edu.keuda.fi>
2024-10-30 09:38:22 +0200

    Pitäis toimia

--- 1A/kirjautumislomake.js
+++ 1A/kirjautumislomake.js
@@ -4,8 +4,12 @@ function tarkista(){
     let salaSana = document.getElementById("salasana").value
     let sahkoPosti = document.getElementById("sähköposti").value
     let palaute = document.getElementById("textarea").value
-    let checkBox1 = document.querySelector("radio").checked
-    let checkBox2 = document.querySelector("checkbox").checked
+    let radio1 = document.getElementById("radio1").checked
+    let radio2 = document.getElementById("radio2").checked
+    let checkBox1 = document.getElementById("checkbox1").checked
+    let checkBox2 = document.getElementById("checkbox2").checked
+    let checkBox3 = document.getElementById("checkbox3").checked
+    let checkBox4 = document.getElementById("checkbox4").checked
 
     if (etuNimi.trim() == ""){
         alert("Etunimi puuttuu")
@@ -19,14 +23,14 @@ function tarkista(){
     else if (palaute.trim() == ""){
         alert("Palaute on tyhjä")
     }
-    else if (checkBox1 == false){
+    else if (radio1 || radio2 == false){
         alert("Valitse jotain")
     }
-    else if (checkBox2 == false){
-        alert("Valitse jotain")
+    else if (checkBox1 || checkBox2 || checkBox3 || checkBox4 == true){
+        alert("Lähetetty")
     }
     else{
-        alert("Lähetetty")
+        alert("Jotain puuttuu")
     }
     event.preventDefault();
 }
