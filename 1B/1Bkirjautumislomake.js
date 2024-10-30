7c3854cf3916afc5057c1d1f23d029c288e1b697
aku-koivunen <aku.koivunen@edu.keuda.fi>
2024-10-30 10:57:30 +0200

    korjailua

--- 1B/1Bkirjautumislomake.js
+++ 1B/1Bkirjautumislomake.js
@@ -8,34 +8,26 @@ function tarkista() {
   let checkBox1 = document.getElementById("checkbox1").checked;
   let checkBox2 = document.getElementById("checkbox2").checked;
   let checkBox3 = document.getElementById("checkbox3").checked;
+  let textara = document.getElementById("textara").value;
 
   if (nimi.trim() == "") {
     alert("Anna nimi");
   } else if (3 > nimi.lenght) {
     alert("Nimi on liian lyhyt");
-  }
-  if (email.trim() == "") {
+  } else if (email.trim() == "") {
     alert("Anna sähköposti");
-  } else if (emailIsValid(sahkoposti)) {
-  } else {
-    alert("Anna oikea sÃ¤hkÃ¶postiosoitteesi");
-    lomake.email.focus();
-    return false;
-  }
-  if (ika.trim() == "") {
+  } else if (ika.trim() == "") {
     alert("Anna ikä");
   } else if (isNaN(ika) != false) {
     alert("Iässä ei saa olla muuta kuin numeroita!");
-  }
-  if (radio1 == false && radio2 == false) {
+  } else if (radio1 == false && radio2 == false) {
     alert("Valitse pallukoita");
-  }
-  if (checkBox1 == false && checkBox2 == false && checkBox3 == false) {
+  } else if (checkBox1 == false && checkBox2 == false && checkBox3 == false) {
     alert("Valitse laatikoita");
+  } else if (textara.trim() == "") {
+    alert("Anna lisäpalautetta :)");
+  } else {
+    alert("Kaikki on lähetetty!");
   }
-  event.preventDefault()
-}
-
-function emailIsValid(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  event.preventDefault();
 }
