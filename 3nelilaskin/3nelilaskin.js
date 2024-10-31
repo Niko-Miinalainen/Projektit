afe5e5e9981a7b12462481bb1f63baab792cc66c
aku-koivunen <aku.koivunen@edu.keuda.fi>
2024-10-31 10:06:08 +0200

    js aloitus

---
+++ 3nelilaskin/3nelilaskin.js
@@ -0,0 +1,49 @@
+let luku = [];
+let functio = [];
+let näyttö = "";
+
+function naytto() {
+  näyttö = "";
+  for (let i = 0; 0 < luku.length; i++) {
+    näyttö += luku[i];
+    if (functio.length > i) {
+      näyttö += functio[i];
+    }
+  }
+  document.getElementById("taulu").innerHTML = näyttö;
+}
+
+function resetoi() {
+  luku.length = 0;
+  functio.length = 0;
+  näyttö = "";
+  document.getElementById("taulu").innerHTML = "";
+}
+
+function miniresetoi() {
+  if (luku.length <= functio.length) {
+    functio.pop();
+  } else {
+    luku.pop();
+  }
+  naytto();
+}
+
+function toiminto(funktio) {
+  if (functio.length < luku.length) {
+    functio.push(funktio);
+    naytto();
+  }
+}
+
+function yhtasuuri() {}
+
+function numeronlisays(num) {
+  if (luku.length > functio.length || luku.length == 0) {
+    luku.push(num);
+    naytto();
+  } else {
+    luku[luku.length] += num;
+    naytto();
+  }
+}
