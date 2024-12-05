11a6a694a7c6465d58586928755cceeddfee7d8a
aku-koivunen <aku.koivunen@edu.keuda.fi>
2024-12-05 13:37:52 +0200

    Toimiva nimipäivä kalenteri

--- 9nimipäivä/9nimipäivä.js
+++ 9nimipäivä/9nimipäivä.js
@@ -1,9 +1,20 @@
+let maa = "fi";
+
+function valitse() {
+  maa = document.getElementById("maa").value;
+  tänään();
+  huomenna();
+  eilen();
+  päivänä();
+  console.log(maa);
+}
+
 tänään();
 function tänään() {
   const url = new URL("https://nameday.abalin.net/api/V1/today");
 
   const params = {
-    country: "fi",
+    country: maa,
   };
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key])
@@ -21,8 +32,15 @@ function tänään() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("t").innerHTML =
-        data.day + "." + data.month + " " + data.nameday.fi;
+        data.day + "." + data.month + " " + data.nameday[maa];
     });
+
+  console.log(
+    fetch(url, {
+      method: "GET",
+      headers,
+    }).then((response) => response.json())
+  );
 }
 
 huomenna();
@@ -30,7 +48,7 @@ function huomenna() {
   const url = new URL("https://nameday.abalin.net/api/V1/tomorrow");
 
   const params = {
-    country: "fi",
+    country: maa,
   };
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key])
@@ -48,7 +66,7 @@ function huomenna() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("h").innerHTML =
-        data.day + "." + data.month + " " + data.nameday.fi;
+        data.day + "." + data.month + " " + data.nameday[maa];
     });
 }
 
@@ -57,7 +75,7 @@ function eilen() {
   const url = new URL("https://nameday.abalin.net/api/V1/yesterday");
 
   const params = {
-    country: "fi",
+    country: maa,
   };
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key])
@@ -75,20 +93,49 @@ function eilen() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("e").innerHTML =
-        data.day + "." + data.month + " " + data.nameday.fi;
+        data.day + "." + data.month + " " + data.nameday[maa];
     });
 }
 
 päivänä();
 function päivänä() {
   let aika = document.getElementById("päivä").value.split("-");
-  console.log(aika);
+
   const url = new URL("https://nameday.abalin.net/api/V1/getdate");
 
+  let params = {
+    day: aika[2],
+    month: aika[1],
+    country: maa,
+  };
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+  console.log(aika);
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("p").innerHTML =
+        data.day + "." + data.month + " " + data.nameday[maa];
+    });
+}
+
+function nimellä() {
+  let nimi = document.getElementById("nimi").value;
+
+  const url = new URL("https://nameday.abalin.net/api/V1/getname");
+
   const params = {
-    day: "18",
-    month: "11",
-    country: "hu",
+    name: nimi,
+    country: maa,
   };
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key])
@@ -100,7 +147,20 @@ function päivänä() {
   };
 
   fetch(url, {
-    method: "POST",
+    method: "GET",
     headers,
-  }).then((response) => response.json());
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("n").innerHTML = "";
+      for (let o = 0; o < data[0].length; o++) {
+        document.getElementById("n").innerHTML +=
+          data[0][o].day +
+          "." +
+          data[0][o].month +
+          " " +
+          data[0][o].name +
+          "<br>";
+      }
+    });
 }
